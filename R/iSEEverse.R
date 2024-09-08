06c8afd1b8cb0397554de12133399bb12ad3f690
Federico Marini <marinif@uni-mainz.de>
2024-09-08 16:15:20 +0200

    logo and update function stubs

---
+++ R/iSEEverse.R
@@ -0,0 +1,55 @@
+
+
+#' Update iSEE and the whole iSEEverse
+#'
+#' @param repos The repositories to use to check for updates.
+#' Defaults to getOption("repos").
+#'
+#' @return An invisible NULL or the suggested commands to update the
+#' corresponding packages.
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' iSEEverse_update()
+#' }
+iSEEverse_update <- function(repos = getOption("repos")) {
+  bmv <- BiocManager::valid(pkgs = all_iSEE_pkgs)
+
+  if(bmv) {
+    cli::cat_line("All iSEEverse packages are up-to-date!")
+    return(invisible())
+  } else {
+    # suggest command to install the not up-to-date ones
+  }
+}
+
+
+#' The iSEEverse logo, using ASCII characters
+#'
+#' @details
+#' Generated via https://patorjk.com/software/taag/#p=display&f=Big&t=iSEE
+#'
+#' @return The character vector to be parsed in the startup message
+#'
+#' @export
+#'
+#' @examples
+#' iSEEverse_logo()
+iSEEverse_logo <- function() {
+  logo <- c(
+    " _  _____ ______ ______ ",
+    "(_)/ ____|  ____|  ____|",
+    " _| (___ | |__  | |__  ",
+    "| |\\___ \\|  __| |  __|  ",
+    "| |____) | |____| |____ ",
+    "|_|_____/|______|______|"
+  )
+
+  # structure(logo, class = "iSEEverse_logo")
+  # message(logo, sep = "\n")
+  # invisible(NULL)
+
+  return(paste(logo, collapse = "\n"))
+}
