912f6a75fe7e89532820587eb801be42d2bf9487
Federico Marini <marinif@uni-mainz.de>
2024-09-08 16:13:42 +0200

    adding R file for pkg docs and imports

---
+++ R/iSEEverse-pkg.R
@@ -0,0 +1,21 @@
+#' iSEEverse
+#'
+#' @name iSEEverse-pkg
+#' @docType package
+#' @keywords internal
+#'
+#' @import iSEE
+#' @import iSEEu
+#' @import iSEEhex
+#' @import iSEEde
+#' @import iSEEpathways
+#' @import iSEEhub
+#' @import iSEEindex
+#' @import iSEEfier
+#' @import iSEEid
+#'
+#' @importFrom cli col_green col_blue ansi_nchar ansi_align
+#' @importFrom utils packageVersion
+#' @importFrom BiocManager valid
+#'
+"_PACKAGE"
